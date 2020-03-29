750325b64eabdab6e304eae98ac15d6ba686a589
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-03-29 15:37:04 +0200

    ZenHub: Fix Music Visualizer

--- src/com/zenx/zen/hub/fragments/lockscreenambient/tabs/submodules/Visualizer.java
+++ src/com/zenx/zen/hub/fragments/lockscreenambient/tabs/submodules/Visualizer.java
@@ -17,28 +17,41 @@
 package com.zenx.zen.hub.fragments.lockscreenambient.tabs.submodules;
 
 import android.content.ContentResolver;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
-import androidx.preference.ListPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceCategory;
 
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.zenx.support.preferences.SecureSettingSeekBarPreference;
+import com.zenx.support.preferences.SecureSettingSwitchPreference;
+
 public class Visualizer extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
 
     private static final String KEY_AUTOCOLOR = "lockscreen_visualizer_autocolor";
     private static final String KEY_LAVALAMP = "lockscreen_lavalamp_enabled";
 
-    private SwitchPreference mAutoColor;
-    private SwitchPreference mLavaLamp;
+    private SecureSettingSwitchPreference mAutoColor;
+    private SecureSettingSwitchPreference mLavaLamp;
+    private SecureSettingSwitchPreference mAmbientVisualizer;
+    private SecureSettingSeekBarPreference mColorBlend;
+    private PreferenceCategory mSolidLines;
+
+    private TextView mTextView;
+    private View mSwitchBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,7 +65,7 @@ public class Visualizer extends SettingsPreferenceFragment implements
                 Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED, 1,
                 UserHandle.USER_CURRENT) != 0;
 
-        mAutoColor = (SwitchPreference) findPreference(KEY_AUTOCOLOR);
+        mAutoColor = (SecureSettingSwitchPreference) findPreference(KEY_AUTOCOLOR);
         mAutoColor.setEnabled(!mLavaLampEnabled);
 
         if (mLavaLampEnabled) {
@@ -63,8 +76,62 @@ public class Visualizer extends SettingsPreferenceFragment implements
                     R.string.lockscreen_autocolor_summary));
         }
 
-        mLavaLamp = (SwitchPreference) findPreference(KEY_LAVALAMP);
+        mLavaLamp = (SecureSettingSwitchPreference) findPreference(KEY_LAVALAMP);
         mLavaLamp.setOnPreferenceChangeListener(this);
+
+        mAmbientVisualizer = (SecureSettingSwitchPreference) findPreference("ambient_visualizer");
+        mColorBlend = (SecureSettingSeekBarPreference) findPreference("lockscreen_lavalamp_speed");
+        mSolidLines = (PreferenceCategory) findPreference("lockscreen_solid_lines_category");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.master_setting_switch, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean enabled = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.LOCKSCREEN_VISUALIZER_ENABLED, 0) == 1;
+
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(enabled ?
+                R.string.switch_on_text : R.string.switch_off_text));
+
+        mSwitchBar = view.findViewById(R.id.switch_bar);
+        Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(enabled);
+        switchWidget.setOnCheckedChangeListener(this);
+        mSwitchBar.setActivated(enabled);
+        mSwitchBar.setOnClickListener(v -> {
+            switchWidget.setChecked(!switchWidget.isChecked());
+            mSwitchBar.setActivated(switchWidget.isChecked());
+        });
+
+        mAutoColor.setEnabled(enabled);
+        mLavaLamp.setEnabled(enabled);
+        mAmbientVisualizer.setEnabled(enabled);
+        mColorBlend.setEnabled(enabled);
+        mSolidLines.setEnabled(enabled);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.LOCKSCREEN_VISUALIZER_ENABLED, isChecked ? 1 : 0);
+        mTextView.setText(getString(isChecked ? R.string.switch_on_text : R.string.switch_off_text));
+        mSwitchBar.setActivated(isChecked);
+
+        mAutoColor.setEnabled(isChecked);
+        mLavaLamp.setEnabled(isChecked);
+        mAmbientVisualizer.setEnabled(isChecked);
+        mColorBlend.setEnabled(isChecked);
+        mSolidLines.setEnabled(isChecked);
     }
 
     @Override
