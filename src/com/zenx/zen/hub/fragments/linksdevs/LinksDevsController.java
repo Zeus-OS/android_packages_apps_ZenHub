36f5f9d0c94d7b6ff955c13678f7e4b35a49048f
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-06-06 15:31:22 +0200

    ZenHub: Add Rom Info section

--- src/com/zenx/zen/hub/fragments/linksdevs/LinksDevsController.java
+++ src/com/zenx/zen/hub/fragments/linksdevs/LinksDevsController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.zenx.zen.hub.fragments.noficationdevs;
+package com.zenx.zen.hub.fragments.linksdevs;
 
 import android.os.Bundle;
 import android.content.Context;
