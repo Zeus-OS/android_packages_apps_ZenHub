aff0b8c0866be74c4710a21f3a666c284f7ab52d
TikkiX2 <radstylemusic@gmail.com>
2020-09-07 11:11:04 +0200

    Add Media Card in Ambient [2/2]

--- src/com/zenx/zen/hub/fragments/screenanimation/tabs/submodules/AudioPanel.java
+++ src/com/zenx/zen/hub/fragments/screenanimation/tabs/submodules/AudioPanel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.zenx.zen.hub.fragments.screenanimation.tabs;
+package com.zenx.zen.hub.fragments.screenanimation.tabs.submodules;
 
 import android.app.Activity;
 import android.content.Context;
